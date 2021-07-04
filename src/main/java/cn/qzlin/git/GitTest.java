627ccd768bcbdadccc9d4b4d9444a4cedbd2018d
qzl <q_zlin@foxmail.com>
2021-07-04 11:51:12 +0800

    first commit

---
+++ src/main/java/cn/qzlin/git/GitTest.java
@@ -0,0 +1,15 @@
+package cn.qzlin.git;
+
+/**
+ * @description:
+ * @author: qzl
+ * @created: 2021/07/04 11:47
+ */
+
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello test!");
+    }
+}
