f259180c636f5a1dac1e88af9b8f94ee7221c4dc
qzl <q_zlin@foxmail.com>
2021-07-04 20:27:26 +0800

    push Github test

--- src/main/java/cn/qzlin/git/GitTest.java
+++ src/main/java/cn/qzlin/git/GitTest.java
@@ -20,5 +20,6 @@ public class GitTest {
         System.out.println("hot-fix 分支");
         System.out.println("master test!!!");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
